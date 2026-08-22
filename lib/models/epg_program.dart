a587e327a57e9a66b52778e0b0f6fe0cacd99494
9602894 <9602894@qq.com>
2026-08-22 08:41:42 +0800

    Update epg_program.dart

--- lib/models/epg_program.dart
+++ lib/models/epg_program.dart
@@ -1,8 +1,27 @@
 class EpgProgram {
+  final String title;
   final DateTime start;
   final DateTime end;
-  final String title;
   final String? desc;
 
-  EpgProgram({required this.start, required this.end, required this.title, this.desc});
+  EpgProgram({
+    required this.title,
+    required this.start,
+    required this.end,
+    this.desc,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'title': title,
+    'start': start.millisecondsSinceEpoch,
+    'end': end.millisecondsSinceEpoch,
+    'desc': desc,
+  };
+
+  factory EpgProgram.fromJson(Map<String, dynamic> json) => EpgProgram(
+    title: json['title'] as String,
+    start: DateTime.fromMillisecondsSinceEpoch(json['start'] as int),
+    end: DateTime.fromMillisecondsSinceEpoch(json['end'] as int),
+    desc: json['desc'] as String?,
+  );
 }
