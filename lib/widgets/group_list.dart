fd83fbd4079c91a427260362580c82287635e8ac
9602894 <9602894@qq.com>
2026-08-27 07:12:17 +0800

    Update group_list.dart

--- lib/widgets/group_list.dart
+++ lib/widgets/group_list.dart
@@ -5,12 +5,7 @@ class GroupList extends StatelessWidget {
   final String? selectedGroup;
   final ValueChanged<String> onSelect;
 
-  const GroupList({
-    Key? key,
-    required this.groups,
-    this.selectedGroup,
-    required this.onSelect,
-  }) : super(key: key);
+  const GroupList({Key? key, required this.groups, this.selectedGroup, required this.onSelect}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,25 +20,12 @@ class GroupList extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
             decoration: BoxDecoration(
               color: isSelected ? Colors.blue.withOpacity(0.3) : Colors.transparent,
-              border: Border(
-                left: BorderSide(
-                  color: isSelected ? Colors.yellow : Colors.transparent,
-                  width: 3,
-                ),
-              ),
+              border: Border(left: BorderSide(color: isSelected ? Colors.yellow : Colors.transparent, width: 3)),
             ),
-            child: Text(
-              group,
-              style: TextStyle(
-                color: isSelected ? Colors.yellow : Colors.white,
-                fontSize: 14,
-                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-              ),
-            ),
+            child: Text(group, style: TextStyle(color: isSelected ? Colors.yellow : Colors.white, fontSize: 14, fontWeight: isSelected ? FontWeight.bold : FontWeight.normal)),
           ),
         );
       },
     );
   }
 }
-
