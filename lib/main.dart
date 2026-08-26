2b75d78e3a26d9823df262df4cf73b3d7f441c80
9602894 <9602894@qq.com>
2026-08-27 07:10:39 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,11 @@ import 'services/log_service.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LogService.init();
-
-  // TV/机顶盒焦点策略：始终显示焦点高亮
   FocusManager.instance.highlightStrategy = FocusHighlightStrategy.alwaysTouch;
-
-  // 强制横屏
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]);
-
-  // 捕获错误
   FlutterError.onError = (FlutterErrorDetails details) {
     FlutterError.dumpErrorToConsole(details);
     LogService.writeCrashLog(details.exception, details.stack);
@@ -27,13 +21,11 @@ void main() async {
     LogService.writeCrashLog(error, stack);
     return true;
   };
-
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
