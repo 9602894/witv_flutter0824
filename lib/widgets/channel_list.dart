d14c0aba2341dbedab05190dbc311a96c3eb3253
9602894 <9602894@qq.com>
2026-08-26 14:39:22 +0800

    Update channel_list.dart

--- lib/widgets/channel_list.dart
+++ lib/widgets/channel_list.dart
@@ -1,94 +1,7 @@
-import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import '../models/channel.dart';
 import '../models/epg_program.dart';
-import '../services/logo_service.dart';
-
-class ChannelLogo extends StatefulWidget {
-  final String channelName;
-  final double width;
-  final double height;
-  final BoxFit fit;
-  final Color? backgroundColor;
-
-  const ChannelLogo({
-    Key? key,
-    required this.channelName,
-    this.width = 32,
-    this.height = 32,
-    this.fit = BoxFit.contain,
-    this.backgroundColor,
-  }) : super(key: key);
-
-  @override
-  State<ChannelLogo> createState() => _ChannelLogoState();
-}
-
-class _ChannelLogoState extends State<ChannelLogo> {
-  Uint8List? _logoBytes;
-  VoidCallback? _logoListener;
-
-  @override
-  void initState() {
-    super.initState();
-    _load();
-    _logoListener = () {
-      if (mounted) _load();
-    };
-    LogoService().logoUpdateNotifier.addListener(_logoListener!);
-  }
-
-  @override
-  void didUpdateWidget(ChannelLogo oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.channelName != widget.channelName) {
-      if (mounted) setState(() => _logoBytes = null);
-      _load();
-    }
-  }
-
-  @override
-  void dispose() {
-    if (_logoListener != null) {
-      LogoService().logoUpdateNotifier.removeListener(_logoListener!);
-    }
-    super.dispose();
-  }
-
-  Future<void> _load() async {
-    final service = LogoService();
-    final file = await service.getLogo(widget.channelName);
-    if (mounted && file != null && file.existsSync()) {
-      final bytes = await file.readAsBytes();
-      setState(() => _logoBytes = bytes);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_logoBytes != null && _logoBytes!.isNotEmpty) {
-      return Container(
-        width: widget.width,
-        height: widget.height,
-        color: widget.backgroundColor ?? Colors.transparent,
-        child: Image.memory(
-          _logoBytes!,
-          width: widget.width,
-          height: widget.height,
-          fit: widget.fit,
-          gaplessPlayback: true,
-          errorBuilder: (_, __, ___) => _buildPlaceholder(),
-        ),
-      );
-    }
-    return _buildPlaceholder();
-  }
-
-  Widget _buildPlaceholder() {
-    return SizedBox(width: widget.width, height: widget.height);
-  }
-}
+import 'channel_logo.dart';
 
 class ChannelList extends StatelessWidget {
   final List<Channel> channels;
@@ -104,7 +17,7 @@ class ChannelList extends StatelessWidget {
     required this.selectedChannel,
     required this.onSelect,
     required this.epgMap,
-    this.showChannelNumber = true,
+    this.showChannelNumber = true,   // 改为 true
     this.showLogo = true,
   }) : super(key: key);
 
@@ -119,43 +32,12 @@ class ChannelList extends StatelessWidget {
         return ListTile(
           dense: true,
           leading: showLogo
-              ? Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    if (showChannelNumber && channel.number != null)
-                      Container(
-                        width: 40,
-                        alignment: Alignment.center,
-                        child: Text(
-                          '${channel.number}',
-                          style: TextStyle(
-                            color: isSelected ? Colors.yellow : Colors.white70,
-                            fontSize: 13,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ChannelLogo(
-                      channelName: channel.name,
-                      width: 32,
-                      height: 32,
-                    ),
-                  ],
+              ? ChannelLogo(
+                  channelName: channel.name,
+                  width: 32,
+                  height: 32,
                 )
-              : (showChannelNumber && channel.number != null)
-                  ? Container(
-                      width: 40,
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${channel.number}',
-                        style: TextStyle(
-                          color: isSelected ? Colors.yellow : Colors.white70,
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    )
-                  : null,
+              : null,
           title: Text(
             showChannelNumber && channel.number != null
                 ? '${channel.number}. ${channel.name}'
