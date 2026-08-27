46f0a7c7c3a5f8bbd9575707c14a45282138a607
9602894 <9602894@qq.com>
2026-08-27 08:46:23 +0800

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/whyun/witv/MainActivity.kt
+++ android/app/src/main/kotlin/com/whyun/witv/MainActivity.kt
@@ -6,7 +6,6 @@ import io.flutter.embedding.engine.FlutterEngine
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        // 注意：IjkPlayerViewFactory 的构造函数无参数，不要传入任何东西
         flutterEngine.platformViewsController.registry.registerViewFactory("IjkPlayer", IjkPlayerViewFactory())
     }
 }
