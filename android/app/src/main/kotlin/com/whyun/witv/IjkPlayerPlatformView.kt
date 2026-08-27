9ee3d5320e1ff3f25cf678d929b6021efb1b6246
9602894 <9602894@qq.com>
2026-08-27 08:04:28 +0800

    Update IjkPlayerPlatformView.kt

--- android/app/src/main/kotlin/com/whyun/witv/IjkPlayerPlatformView.kt
+++ android/app/src/main/kotlin/com/whyun/witv/IjkPlayerPlatformView.kt
@@ -20,20 +20,25 @@ class IjkPlayerPlatformView(
         IjkMediaPlayer.native_profileBegin("libijkplayer.so")
 
         player = IjkMediaPlayer().apply {
+            // ---------- 通用选项 ----------
             setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "opensles", 0L)
             setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "overlay-format", IjkMediaPlayer.SDL_FCC_RV32.toLong())
             setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 5L)
             setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "start-on-prepared", 1L)
             setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "probesize", 1024 * 1024L)
-            setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzeduration", 3 * 1000 * 1000L)
+            setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzeduration", 1000 * 1000L)
 
-            // 解码器配置
+            // ---------- 解码器配置 ----------
             if (decoderIndex == 0) {
+                // 硬解码
                 Log.i("IJKPlayerPlatform", "启用硬解码")
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec", 1L)
+                setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-avc", 1L)
+                setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-hevc", 1L)
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-auto-rotate", 1L)
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-handle-resolution-change", 1L)
             } else {
+                // 软解码
                 Log.i("IJKPlayerPlatform", "启用软解码")
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec", 0L)
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-all-videos", 0L)
