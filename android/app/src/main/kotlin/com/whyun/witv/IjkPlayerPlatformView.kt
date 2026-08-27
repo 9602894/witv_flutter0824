69bb443c3aae5516e26f3d387f5669f5d7a1c0fd
9602894 <9602894@qq.com>
2026-08-27 08:52:38 +0800

    Update IjkPlayerPlatformView.kt

--- android/app/src/main/kotlin/com/whyun/witv/IjkPlayerPlatformView.kt
+++ android/app/src/main/kotlin/com/whyun/witv/IjkPlayerPlatformView.kt
@@ -1,11 +1,11 @@
 package com.whyun.witv
 
 import android.content.Context
+import android.util.Log
 import android.view.SurfaceView
 import android.view.View
 import io.flutter.plugin.platform.PlatformView
 import tv.danmaku.ijk.media.player.IjkMediaPlayer
-import android.util.Log
 
 class IjkPlayerPlatformView(
     context: Context,
@@ -22,25 +22,27 @@ class IjkPlayerPlatformView(
         player = IjkMediaPlayer().apply {
             setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "opensles", 0L)
             setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "overlay-format", IjkMediaPlayer.SDL_FCC_RV32.toLong())
-            setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 5L)
-            setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "start-on-prepared", 1L)
+            setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 1L)
+            setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "render-wait-start", 0L)
+            setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "max-buffer-size", 2 * 1024 * 1024L)
             setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "probesize", 1024 * 1024L)
             setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzeduration", 1000 * 1000L)
+            setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "flush_packets", 1L)
 
             if (decoderIndex == 0) {
                 Log.i("IJKPlayerPlatform", "启用硬解码")
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec", 1L)
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-avc", 1L)
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-hevc", 1L)
-                setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-auto-rotate", 1L)
-                setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-handle-resolution-change", 1L)
+                setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-auto-rotate", 0L)
+                setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-handle-resolution-change", 0L)
+                setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-timeout", 3000L)
             } else {
                 Log.i("IJKPlayerPlatform", "启用软解码")
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec", 0L)
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-all-videos", 0L)
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-avc", 0L)
                 setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-hevc", 0L)
-                setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "rtsp_transport", "tcp")
             }
 
             dataSource = url
